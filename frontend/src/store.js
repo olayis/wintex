ae0213c740a11ee8e36aaa47da251cba8e565576
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-08-18 04:26:45 +0100

    implement product state management

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/productReducers';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+});
 
 const initialState = {};
 
