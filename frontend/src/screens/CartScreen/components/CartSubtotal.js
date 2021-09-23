5790b750f2644bc8ce2323ff1dcde589c1001d0e
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-09-23 18:39:25 +0100

    Add CartScreen and ProductScreen components folders to resolve folders name case sensitivity issue

---
+++ frontend/src/screens/CartScreen/components/CartSubtotal.js
@@ -0,0 +1,67 @@
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import numeral from 'numeral';
+
+const CartSubtotal = ({
+  classes,
+  cartItems,
+  cartSubtotalCount,
+  cartSubtotalPrice,
+  checkoutHandler,
+  continueShoppingHandler,
+}) => {
+  return (
+    <Paper className={classes.paper}>
+      <List aria-label='cart subtotal'>
+        <ListItem>
+          <Typography variant='h2'>
+            Subtotal ({cartSubtotalCount}){' '}
+            {cartSubtotalCount === 1 ? 'item' : 'items'}
+          </Typography>
+        </ListItem>
+        <Divider />
+
+        <ListItem>
+          <Typography variant='h6' className={classes.boldFont}>
+            {numeral(cartSubtotalPrice).format('$0,0.00')}
+          </Typography>
+        </ListItem>
+        <Divider />
+
+        <ListItem>
+          <Button
+            variant='contained'
+            color='primary'
+            size='large'
+            className={classes.buttonBlock}
+            disabled={cartItems.length === 0}
+            onClick={checkoutHandler}
+          >
+            Proceed To Checkout
+          </Button>
+        </ListItem>
+
+        <ListItem>
+          <Button
+            variant='outlined'
+            color='primary'
+            size='large'
+            className={classes.buttonBlock}
+            style={{
+              marginTop: '5px',
+            }}
+            onClick={continueShoppingHandler}
+          >
+            Continue Shopping
+          </Button>
+        </ListItem>
+      </List>
+    </Paper>
+  );
+};
+
+export default CartSubtotal;
