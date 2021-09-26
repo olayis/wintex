7e087e08145fbfc9e71612c57728015085f8bee0
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-09-26 03:30:05 +0100

    Modify Disclaimer message

--- frontend/src/components/Misc/Disclaimer.js
+++ frontend/src/components/Misc/Disclaimer.js
@@ -5,8 +5,9 @@ const Disclaimer = () => {
     <div style={{ margin: '65px 0 12px 0' }}>
       <Message collapsible disappear timeout={10000}>
         <strong>
-          Disclaimer: This is not an actual online store, the products are
-          thereby not purchasable.
+          Disclaimer: This is not an actual online store, the products and
+          associated entities being displayed are not real and thereby not
+          purchasable.
         </strong>
       </Message>
     </div>
