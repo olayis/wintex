95220b93bd29807cfacfad6844115f38306af1e1
OlusegunMiles <olayinkasegunsolo@gmail.com>
2021-09-26 02:39:06 +0100

    Change timeout for disclamer to 10s

--- frontend/src/components/Misc/Disclaimer.js
+++ frontend/src/components/Misc/Disclaimer.js
@@ -3,7 +3,7 @@ import Message from '../Message/Message';
 const Disclaimer = () => {
   return (
     <div style={{ margin: '65px 0 12px 0' }}>
-      <Message collapsible disappear>
+      <Message collapsible disappear timeout={10000}>
         <strong>
           Disclaimer: This is not an actual online store, the products are
           thereby not purchasable.
